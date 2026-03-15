3d10efa2ccb935f8f6bd159b24840cecd1370ef8
IvanCarl <ivancarldmanimtim@iskolarngbayan.pup.edu.ph>
2026-03-15 10:47:16 +0800

    Removed redundant comments

--- accounts.h
+++ accounts.h
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 
 #pragma once
 
@@ -13,5 +6,5 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <fstream>
 #include <string>
 using namespace std;
-
+// declaration to reuse
 bool haveAcc(string username, string pass);
